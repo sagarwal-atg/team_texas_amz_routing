56694f7904d2f6972a3b54d7fbf885be406f0167
Josiah Coad <josiah@coad.net>
2021-04-19 12:59:26 -0500

    big code cleanup and added zone_crossing feature

---
+++ dataloaders/data.py
@@ -0,0 +1,123 @@
+
+from types import SimpleNamespace
+import json
+from typing import Dict, List, Union
+import numpy as np
+from munch import Munch
+
+class RouteDatum:
+    SCORE = SimpleNamespace(LOW='Low', MEDIUM='Medium', HIGH='High')
+
+    def __init__(self, data):
+        self._data = Munch(data)
+
+    def get_stops(self, stop_ids: List[str]=None):
+        stops = self._data.stops
+        if stop_ids:
+            stops = {key: stops[key] for key in stop_ids}
+        return stops
+
+    def get_zones(self, stop_ids: List[str]=None) -> Dict[str, str]:
+        """
+        Returns: a map of zone id's for each stop
+            ex: {'A': 'zone1', 'B': 'zone2', 'C': 'zone3'}
+        """
+        return {key: stop['zone_id'] for key, stop in self.get_stops(stop_ids).items()}
+
+    def get_score(self):
+        return self._data.route_score
+
+    def is_high_score(self):
+        return self.get_score() == self.SCORE.HIGH
+
+class SequenceDatum:
+    """
+    Data about the actual route taken by the driver
+    """
+    def __init__(self, data: Dict[str, Dict[str, int]]):
+        """
+        data: a dict whose values indicate the order we visited the stops
+            ex: {'actual': {'A': 0, 'B': 2, 'C': 1}}
+        """
+        self._stops = data['actual']
+
+    def get_stop_ids(self) -> List[str]:
+        """
+        Returns a list of stops in the order they come in the data
+            ex: ['A', 'B', 'C']
+        """
+        return list(self._stops.keys())
+
+    def _get_actual_order(self) -> List[str]:
+        """
+        Returns: a list ordered by the values in stop_sequence
+            ex: ['A', 'C', 'B']
+        """
+        return sorted(self._stops, key=lambda k: self._stops[k])
+
+    def get_route_order(self):
+        """
+        stop_sequence: a dict whose values indicate the order we visited the stops
+            ex: {'A': 0, 'B': 2, 'C': 1}
+        Returns: a list of ints whose index is the FROM index in stop_sequence and whose value is
+            the TO index in stop_sequence.
+            ex: [2, 0, 1] (meaning that we go from [A->C, B->A, C->B])
+        """
+        stops_ordered = self._get_actual_order()
+        next_stop = {stop: next_stop for stop, next_stop in zip(np.roll(stops_ordered, 1), stops_ordered)}
+        stop_idx = {k: i for i, k in enumerate(self._stops)}
+        labels = [stop_idx[next_stop[stop]] for stop in stop_idx.keys()] 
+        return labels
+
+    def __len__(self):
+        return len(self.get_stop_ids())
+
+
+class TravelTimeDatum:
+    def __init__(self, data: Dict[str, Dict[str, float]]) -> None:
+        self._data = data
+
+    def as_matrix(self, stop_ids):
+        route_len = len(stop_ids)
+        times = np.zeros((route_len, route_len))
+        for i, stop1 in enumerate(stop_ids):
+            for j, stop2 in enumerate(stop_ids):
+                times[i][j] = self._data[stop1][stop2]
+        return times
+
+
+class AmazonData:
+    def __init__(self, data, constructor: Union[RouteDatum, SequenceDatum]):
+        self._data = {route_id: constructor(value) for route_id, value in data.items()}
+
+    @classmethod
+    def from_file(cls, filepath: str):
+        with open(filepath) as f:
+            data = json.load(f)
+        return cls(data)
+
+    def get_route(self, route_id):
+        return self._data[route_id]
+
+    def __len__(self):
+        return len(self._data)
+
+    def __getitem__(self, route_id):
+        return self.get_route(route_id)
+
+
+class SequenceData(AmazonData):
+    def __init__(self, data):
+        super().__init__(data, SequenceDatum)
+
+
+class RouteData(AmazonData):
+    def __init__(self, data):
+        super().__init__(data, RouteDatum)
+
+    def get_high_score_ids(self):
+        return [route_id for route_id, route in self._data.items() if route.is_high_score()]
+
+class TravelTimeData(AmazonData):
+    def __init__(self, data):
+        super().__init__(data, TravelTimeDatum)
--- dataloaders/irl_dataset.py
+++ dataloaders/irl_dataset.py
@@ -1,40 +1,21 @@
-import json
-from typing import Dict, List, Sequence
+from typing import List, Any
 import numpy as np
 from numpy import concatenate as concat
-from collections import namedtuple
-from sklearn.preprocessing import MinMaxScaler
-import enum
-
+from nptyping import NDArray
 import torch
 from torch.utils.data import Dataset
 
+from .data import RouteData, SequenceData, TravelTimeData, RouteData, RouteDatum, TravelTimeDatum
 
-def order_stops(stop_sequence: Dict[str, int]) -> List[str]:
-    """
-    stop_sequence: a dict whose values indicate the order we visited the stops
-        ex: {'A': 0, 'B': 2, 'C': 1}
-    Returns: a list ordered by the values in stop_sequence
-        ex: ['A', 'C', 'B']
-    """
-    return sorted(stop_sequence, key=lambda k: stop_sequence[k])
+IntMatrix = NDArray[(Any, Any), np.int32]
+FloatMatrix = NDArray[(Any, Any), np.float]
+BoolMatrix = NDArray[(Any, Any), np.bool]
 
-def get_route_order(stop_sequence: Dict[str, int]):
-    """
-    stop_sequence: a dict whose values indicate the order we visited the stops
-        ex: {'A': 0, 'B': 2, 'C': 1}
-    Returns: a list of ints whose index is the FROM index in stop_sequence and whose value is
-        the TO index in stop_sequence.
-        ex: [2, 0, 1] (meaning that we go from [A->C, B->A, C->B])
-    """
-    stops_ordered = order_stops(stop_sequence)
-    next_stop = {stop: next_stop for stop, next_stop in zip(np.roll(stops_ordered, 1), stops_ordered)}
-    stop_ids = list(stop_sequence.keys())
-    stop_idx = {k: i for i, k in enumerate(stop_ids)}
-    labels = [stop_idx[next_stop[stop]] for stop in stop_idx.keys()] 
-    return labels
 
-def extract_travel_times(stop_ids: List[str], travel_times: List[List[float]]):
+###################################
+# Feature Extractors - Begin 
+###################################
+def extract_travel_times(travel_times: TravelTimeDatum, stop_ids: List[str]) -> FloatMatrix:
     """
     stop_ids: a list which is in the same order as the labels
         ex: ['A', 'B', 'C']
@@ -42,70 +23,83 @@ def extract_travel_times(stop_ids: List[str], travel_times: List[List[float]]):
         The times are normalized by dividing across the rows so that the sum time from
         stop_ids[i] -> all other stops is 1.
     """
-    stop_idx = {k: i for i, k in enumerate(stop_ids)}
-    route_len = len(stop_ids)
-    times = np.zeros((route_len, route_len))
-    for i in stop_ids:
-        for j in stop_ids:
-            times[stop_idx[i]][stop_idx[j]] = travel_times[i][j]
+    times = travel_times.as_matrix(stop_ids)
     times /= times.sum(axis=1)[:,np.newaxis]
-
     return times
 
-def right_pad(m, width, constant=0):
+def extract_zone_crossings(route_data: RouteDatum, stop_ids: List[str]) -> BoolMatrix:
+    """
+    Returns: A matrix whose ij entry is 1 if there is a zone crossing between
+        stop stop_ids[i] and stop_ids[j] (and 0 otherwise)
+    """
+    # get a map of zone id's for each stop
+    stop_zones = route_data.get_zones(stop_ids)
+    zone_crossing = lambda stop1, stop2: stop_zones[stop1] != stop_zones[stop2]
+    # get a matrix of zone crossings (note: it should be symmetric)
+    zone_crossings = np.zeros((len(stop_ids), len(stop_ids)))
+    for i, stop1 in enumerate(stop_ids):
+        for j, stop2 in enumerate(stop_ids):
+            zone_crossings[i, j] = zone_crossing(stop1, stop2)
+    return zone_crossings
+
+###################################
+# Feature Extractors - End 
+###################################
+
+
+def right_pad2d(m, width, constant=0):
+    assert width >= m.shape[1]
+    m2 = np.ones((m.shape[0], width)) * constant
+    m2[:,:m.shape[1]] = m
+    return m2
+
+def right_pad3d(m, width, constant=0):
     assert width >= m.shape[2]
     m2 = np.ones((m.shape[0], m.shape[1], width)) * constant
-    m2[:,:,:m.shape[-1]] = m
+    m2[:,:,:m.shape[2]] = m
     return m2
 
 class IRLDataset(Dataset):
     def __init__(self, paths, slice_begin=None, slice_end=None):
-        with open(paths.route) as f:
-            route_data = json.load(f)
+        route_data = RouteData.from_file(paths.route)
+        sequence_data = SequenceData.from_file(paths.sequence)
+        travel_time_data = TravelTimeData.from_file(paths.travel_time)
 
-        with open(paths.labels) as f:
-            sequence_data = json.load(f)
-
-        with open(paths.travel_times) as f:
-            travel_time_data = json.load(f)
-
-        # with open(paths.packages) as f:
-        #     package_data = json.load(f)
-
-        route_ids = [k for k, v in route_data.items() if v['route_score'] == 'High']
+        route_ids = route_data.get_high_score_ids()
         route_ids = route_ids[slice(slice_begin, slice_end)]
-        n_routes = len(route_ids)
-        print(f'Using data from {n_routes} routes.')
-        route_lengths = [len(sequence_data[route_id]['actual']) for route_id in route_ids]
-
+        print(f'Using data from {len(route_ids)} routes.')
+        route_lengths = [len(sequence_data[route]) for route in route_ids]
         self.max_route_len = max(route_lengths)
 
         def get_route_features(route_id):
-            vehicle_cap = route_data[route_id]['executor_capacity_cm3']
+            veh_cap = route_data[route_id]._data.executor_capacity_cm3
             # add any other functions here for more route features
-            return np.array([vehicle_cap])
+            return np.array([veh_cap])
 
         def get_link_features(route_id):
             """
-            Returns: a matrix of shape [route_len, route_len, n] where n is the number of features
+            Returns: a matrix of shape [route_len, max_route_len, n] where n is the number of features
             """
-            stop_ids = list(sequence_data[route_id]['actual'].keys())
-            times = extract_travel_times(stop_ids, travel_time_data[route_id])
-            # add any other functions here for more link features
-            return np.array([times])
+            stop_ids = sequence_data[route_id].get_stop_ids()
+
+            times = extract_travel_times(travel_time_data[route_id], stop_ids)
+            times = right_pad2d(times, self.max_route_len, 0)
+
+            zone_crossings = extract_zone_crossings(route_data[route_id], stop_ids)
+            zone_crossings = right_pad2d(zone_crossings, self.max_route_len, 1)
+
+            # add any other functions here for more link features.
+            return np.array([times, zone_crossings])
 
         # Extract features
-        link_features = [get_link_features(route_id) for route_id in route_ids]
+        link_features = concat([
+            get_link_features(route_id) for route_id in route_ids], axis=1)
         route_features = concat([
             get_route_features(route_id) for route_id in route_ids])
 
-        # Make all matrices the same width by adding 1's to the right side
-        link_features = concat([
-            right_pad(matrix, self.max_route_len) for matrix in link_features], axis=1)
-
         # Extract labels
         lables = concat([
-            get_route_order(sequence_data[route_id]['actual']) for route_id in route_ids])
+            sequence_data[route_id].get_route_order() for route_id in route_ids])
 
         # shape(route_date)=[n_routes, num_route_features]
         route_data = np.repeat(route_features, route_lengths, axis=0)
