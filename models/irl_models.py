f11da1e61b94cf3679e4cc6c85321d47c035cc7b
Shubhankar Agarwal <sagrwl11@illinois.edu>
2021-05-31 19:58:29 -0700

    Working NN code

---
+++ models/irl_models.py
@@ -0,0 +1,23 @@
+from os import link
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from IPython import embed
+
+
+class IRLModel(nn.Module):
+    def __init__(self, num_features):
+        super().__init__()
+        self.fc1 = nn.Linear(num_features, num_features)
+        self.fc2 = nn.Linear(num_features, num_features)
+        self.fc3 = nn.Linear(num_features, 1)
+
+        param = torch.zeros((1)).type(torch.FloatTensor)
+        self.lamb = torch.nn.Parameter(param, requires_grad=True)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        y_hat = self.fc3(x)
+        return y_hat
--- train_irl_nn.py
+++ train_irl_nn.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python3
-import os
-import argparse
-import time
-import torch
 import datetime
-from torch.utils.data import DataLoader, random_split
-from tensorboardX import SummaryWriter
-import numpy as np
-from easydict import EasyDict as edict
-import yaml
 import pprint
-from multiprocessing import Pool
+import time
 from functools import partial
+from multiprocessing import Pool
 
-from models.models import LinearModel
-from dataloaders.irl_dataset import IRLDataset
-from training_utils.arg_utils import get_args, setup_training_output
-from tsp_solvers import tsp, constrained_tsp
-from eval_utils.score import score
+import numpy as np
+import torch
+import torch.nn.functional as F
 from IPython import embed
+from tensorboardX import SummaryWriter
+from torch import optim
+
+from dataloaders.irl_dataset import IRLNNDataset, irl_nn_collate, seq_binary_mat
+from eval_utils.score import score
+from models.irl_models import IRLModel
+from training_utils.arg_utils import get_args, setup_training_output
+from tsp_solvers import constrained_tsp
 
 
 def compute_link_cost_seq(time_matrix, link_features, seq):
     time_cost = 0
     feature_cost = np.zeros(link_features.shape[0])
     for idx in range(1, len(seq)):
-        time_cost += time_matrix[seq[idx-1], seq[idx]]
-        feature_cost += link_features[:, seq[idx-1], seq[idx]]
+        time_cost += time_matrix[seq[idx - 1], seq[idx]]
+        feature_cost += link_features[:, seq[idx - 1], seq[idx]]
     time_cost += time_matrix[seq[-1]][seq[0]]
     feature_cost += link_features[:, seq[-1], seq[0]]
     return time_cost, feature_cost
@@ -36,7 +34,7 @@ def compute_time_violation_seq(time_matrix, time_constraints, seq):
     time = 0.0
     violation_down, violation_up = 0, 0
     for idx in range(1, len(seq)):
-        time += time_matrix[seq[idx-1], seq[idx]]
+        time += time_matrix[seq[idx - 1], seq[idx]]
         violation_up += max(0, time - time_constraints[idx][1])
         violation_down += max(0, time_constraints[idx][0] - time)
     return violation_up + violation_down
@@ -51,32 +49,23 @@ def compute_time_violation_seq(time_matrix, time_constraints, seq):
 #########
 
 
-def compute_tsp_seq_for_route(data, theta, lamb):
-    travel_times, link_features, route_features, time_constraints, \
-        stop_ids, travel_time_dict, label = data
+def compute_tsp_seq_for_route(data, lamb):
+    (
+        objective_matrix,
+        travel_times,
+        time_constraints,
+        stop_ids,
+        travel_time_dict,
+        label,
+    ) = data
 
     demo_stop_ids = [stop_ids[j] for j in label]
     demo_stop_ids.append(demo_stop_ids[0])
 
-    ##### Replace the following part with Neural Network -- forward pass
-    num_link_features, num_stops, _ = link_features.shape
-    temp = link_features.reshape(num_link_features, -1).T
-    temp = temp.dot(theta)
-    objective_matrix = temp.reshape(num_stops, num_stops) + 1 * travel_times
-
-    # objective_matrix = forward_pass(travel_times, link_feature, route_features)
-
     ###########
-
     pred_seq = constrained_tsp.constrained_tsp(
-        objective_matrix, travel_times, time_constraints, depot=label[0], lamb=int(lamb))
-
-    ####### Remove this part
-    ## time_cost and feature_cost not needed in computing grad
-    ## since we have replaced it with a neural net
-    pred_time_cost, pred_feature_cost = compute_link_cost_seq(travel_times, link_features, pred_seq)
-    demo_time_cost, demo_feature_cost = compute_link_cost_seq(travel_times, link_features, label)
-    ########
+        objective_matrix, travel_times, time_constraints, depot=label[0], lamb=int(lamb)
+    )
 
     pred_tv = compute_time_violation_seq(travel_times, time_constraints, pred_seq)
     demo_tv = compute_time_violation_seq(travel_times, time_constraints, label)
@@ -86,123 +75,182 @@ def compute_tsp_seq_for_route(data, theta, lamb):
 
     seq_score = score(demo_stop_ids, pred_stop_ids, travel_time_dict)
 
-    return pred_seq, demo_time_cost, demo_feature_cost, demo_tv, \
-        pred_time_cost, pred_feature_cost, pred_tv, seq_score
+    return (
+        pred_seq,
+        demo_tv,
+        pred_tv,
+        seq_score,
+    )
 
 
-def compute_tsp_seq_for_a_batch(batch_data, theta, lamb):
+def compute_tsp_seq_for_a_batch(batch_data, lamb):
     pool = Pool(processes=8)
-    tsp_func = partial(compute_tsp_seq_for_route, theta=theta, lamb=lamb)
+    tsp_func = partial(compute_tsp_seq_for_route, lamb=lamb)
     batch_output = pool.map(tsp_func, batch_data)
     pool.close()
     return batch_output
 
 
-def get_param(batch_demo_time_cost, batch_demo_feature_cost, batch_demo_tv,
-              batch_pred_time_cost, batch_pred_feature_cost, batch_pred_tv,):
-    X1 = batch_demo_feature_cost - batch_pred_feature_cost
-    X2 = np.expand_dims(batch_demo_tv - batch_pred_tv, axis=1)
-    X = np.concatenate((X1, X2), axis=1)
-    y = - (batch_demo_time_cost - batch_pred_time_cost)
-    params = np.linalg.lstsq(X, y, rcond=None)
-    theta = params[0][:-1]
-    lamb = params[0][-1]
-    return theta, lamb
+def irl_loss(batch_output, thetas, tsp_data, model):
+
+    loss = 0.0
+    for route_idx in range(len(batch_output)):
+        pred_seq = batch_output[route_idx][0]
+        demo_tv = batch_output[route_idx][1]
+        pred_tv = batch_output[route_idx][2]
+
+        pred_cost = torch.sum(
+            torch.from_numpy(seq_binary_mat(pred_seq)).type(torch.FloatTensor)
+            @ thetas[route_idx]
+        )
+        demo_cost = torch.sum(
+            torch.from_numpy(tsp_data[route_idx].binary_mat).type(torch.FloatTensor)
+            @ thetas[route_idx]
+        )
+
+        route_loss = F.relu(
+            (demo_cost + model.lamb * demo_tv) - (pred_cost + model.lamb * pred_tv)
+        )
+
+        loss += route_loss
+
+    loss = loss / len(batch_output)
+
+    return loss
 
 
 def fit(model, dataloader, writer, config):
-    theta = np.array([50.0])
-    lamb = 10.0
-    lr = config.learning_rate
-    clock = 0
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+
+    optimizer = optim.Adam(model.parameters(), lr=config.learning_rate)
 
     # loop over the dataset multiple times
     for epoch_idx in range(config.num_train_epochs):
         epoch_score = 0
         epoch_loss = 0
 
-        clock += 1
-
-        print("Theta: {}, Lambda: {}".format(theta, lamb))
-
-        batch_data = [None] * config.batch_size
         for idx, data in enumerate(dataloader):
-            batch_data[idx % config.batch_size] = data
-            if idx % config.batch_size == config.batch_size - 1:
-                start_time = time.time()
-                batch_output = compute_tsp_seq_for_a_batch(batch_data, theta, lamb)
-                res = list(zip(*batch_output))
-                batch_demo_time_cost = np.array(res[1])
-                batch_demo_feature_cost = np.array(res[2])
-                batch_demo_tv = np.array(res[3])
-                batch_pred_time_cost = np.array(res[4])
-                batch_pred_feature_cost = np.array(res[5])
-                batch_pred_tv = np.array(res[6])
-                batch_seq_score = np.array(res[7])
+            start_time = time.time()
+            nn_data, tsp_data = data
 
-                # compute gradient
-                grad_lamb_batch = batch_demo_tv - batch_pred_tv
-                grad_theta_batch = batch_demo_feature_cost - batch_pred_feature_cost
+            stack_nn_data = torch.cat(nn_data, 0)
+            stack_nn_data = stack_nn_data.to(device)
+            obj_matrix = model(stack_nn_data)
 
-                demo_cost = 1 * batch_demo_time_cost + theta.dot(batch_demo_feature_cost.T)
-                pred_cost = 1 * batch_pred_time_cost + theta.dot(batch_pred_feature_cost.T)
+            idx_so_far = 0
+            thetas = []
+            thetas_tensor = []
+            for idx, data in enumerate(tsp_data):
+                route_len = data.travel_times.shape[0]
+                theta = (
+                    obj_matrix[idx_so_far : (idx_so_far + (route_len * route_len))]
+                    .reshape((route_len, route_len))
+                    .detach()
+                    .numpy()
+                )
 
-                loss = max((np.mean(demo_cost) + lamb * np.mean(batch_demo_tv)) -
-                           (np.mean(pred_cost) + lamb * np.mean(batch_pred_tv)), 0)
+                thetas_tensor.append(
+                    obj_matrix[
+                        idx_so_far : (idx_so_far + (route_len * route_len))
+                    ].reshape((route_len, route_len))
+                )
 
-                # update theta and lambda
-                # r = lr / (1 + clock * 0.0005)
-                # lamb -= np.mean(grad_lamb_batch) * r
-                # theta -= np.mean(grad_theta_batch) * r
+                idx_so_far += route_len * route_len
 
-            ##### Replace the following part with Neural Net -- backward pass
-                # Init parameters by finding optimal paramters
-                theta, lamb = get_param(batch_demo_time_cost,
-                                        batch_demo_feature_cost,
-                                        batch_demo_tv,
-                                        batch_pred_time_cost,
-                                        batch_pred_feature_cost,
-                                        batch_pred_tv)
+                thetas.append(theta)
 
-                # we need be very careful about loss computation
-                # loss.backward()
+            batch_data = []
+            for idx, data in enumerate(tsp_data):
+                batch_data.append(
+                    (
+                        thetas[idx],
+                        data.travel_times,
+                        data.time_constraints,
+                        data.stop_ids,
+                        data.travel_time_dict,
+                        data.label,
+                    )
+                )
 
-            #####
+            batch_output = compute_tsp_seq_for_a_batch(
+                batch_data, model.lamb.detach().numpy()
+            )
 
-                mean_score = np.mean(batch_seq_score)
-                epoch_loss += loss
-                epoch_score += mean_score
+            optimizer.zero_grad()
 
-                writer.add_scalar('Train/batch_route_loss', loss, epoch_idx * len(dataloader) + idx)
-                writer.add_scalar('Train/batch_route_score', mean_score, epoch_idx * len(dataloader) + idx)
+            loss = irl_loss(batch_output, thetas_tensor, tsp_data, model)
 
-                print("Epoch: {}, Step: {}, Loss: {:01f}, Score: {:01f}, Time: {}, Theta: {}, Lambda: {}".
-                      format(epoch_idx, int(idx / config.batch_size), loss, mean_score, time.time() - start_time, theta, lamb))
+            loss.backward()
+            optimizer.step()
+
+            epoch_loss += loss.item()
+
+            res = list(zip(*batch_output))
+            batch_seq_score = np.array(res[3])
+
+            mean_score = np.mean(batch_seq_score)
+            epoch_loss += loss.item()
+            epoch_score += mean_score
+
+            writer.add_scalar(
+                "Train/batch_route_loss", loss.item(), epoch_idx * len(dataloader) + idx
+            )
+            writer.add_scalar(
+                "Train/batch_route_score",
+                mean_score,
+                epoch_idx * len(dataloader) + idx,
+            )
+
+            print(
+                "Epoch: {}, Step: {}, Loss: {:01f}, Score: {:01f}, Time: {}, Lambda: {}".format(
+                    epoch_idx,
+                    int(idx / config.batch_size),
+                    loss.item(),
+                    mean_score,
+                    time.time() - start_time,
+                    model.lamb.clone().detach().numpy(),
+                )
+            )
 
         mean_epoch_loss = (epoch_loss * config.batch_size) / (len(dataloader))
         mean_epoch_score = (epoch_score * config.batch_size) / (len(dataloader))
 
-        print("Epoch Loss: {}, Epoch Score: {}".format(mean_epoch_loss, mean_epoch_score))
+        print(
+            "Epoch Loss: {}, Epoch Score: {}".format(mean_epoch_loss, mean_epoch_score)
+        )
 
-        writer.add_scalar('Train/loss', mean_epoch_loss, epoch_idx)
-        writer.add_scalar('Train/score', mean_epoch_score, epoch_idx)
+        writer.add_scalar("Train/loss", mean_epoch_loss, epoch_idx)
+        writer.add_scalar("Train/score", mean_epoch_score, epoch_idx)
 
 
 def main(config):
     torch.manual_seed(config.seed)
     np.random.seed(config.seed)
-    data = IRLDataset(config.data)
+    train_data = IRLNNDataset(config.data)
+    train_loader = torch.utils.data.DataLoader(
+        train_data,
+        batch_size=config.batch_size,
+        collate_fn=irl_nn_collate,
+        num_workers=2,
+    )
+
     writer = SummaryWriter(
-        logdir=config.tensorboard_dir +
-        '/{}_{}_Model'.format(
-            datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),
-            config.name))
-    model = LinearModel(size=2)
-    fit(model, data.x, writer, config)
-    print('Finished Training')
+        logdir=config.tensorboard_dir
+        + "/{}_{}_Model".format(
+            datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), config.name
+        )
+    )
+
+    model = IRLModel(
+        num_features=3,
+    )
+    fit(model, train_loader, writer, config)
+    print("Finished Training")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     config = get_args()
     pprint.pprint(config)
     setup_training_output(config)
